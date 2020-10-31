6a19730eeb9d6e130554f1594f1d51ade3b2c282
Mauro Guardarini <mauro.guardarini@gmail.com>
2020-10-30 21:10:18 -0300

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import PerfilPalestrante from './PerfilPalestrante';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => PerfilPalestrante);
